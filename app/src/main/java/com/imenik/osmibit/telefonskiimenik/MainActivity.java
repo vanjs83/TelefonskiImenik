1827810327deb65153ba7234942935d9cb006114
vanjs83 <tihomir.vanjurek@gmail.com>
2017-06-06 14:29:43 +0200

    proradila funkcija za kreiranje pdf

--- app/src/main/java/com/imenik/osmibit/telefonskiimenik/MainActivity.java
+++ app/src/main/java/com/imenik/osmibit/telefonskiimenik/MainActivity.java
@@ -1,4 +1,5 @@
 package com.imenik.osmibit.telefonskiimenik;
+import android.graphics.Color;
 import android.os.Environment;
 
 import android.support.v7.app.AppCompatActivity;
@@ -7,11 +8,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
+
 
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Element;
+import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.text.Document;
@@ -20,17 +20,19 @@ import com.itextpdf.text.Document;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
+import static com.itextpdf.text.pdf.BidiOrder.PDF;
 
 
 public class MainActivity extends AppCompatActivity  {
 
     //AppCompatActivity
-    EditText mName, mSurname, mNumber, mGroup;
-    Spinner mSpinner;
-    Button mButtSave;
-    public int ID;
-    public String name, surname, telNumber, group;
+    EditText mName, mSurname, mNumber,mGroup;
+    Button mButtSave, mButtPrint;
+    List<Contact> contact;
+    List<Group> group;
+    public String name, surname, telNumber;
     DatabaseHandler db;
 
     @Override
@@ -41,7 +43,9 @@ public class MainActivity extends AppCompatActivity  {
         mName = (EditText) findViewById(R.id.editIme);
         mSurname = (EditText) findViewById(R.id.editPrezime);
         mNumber = (EditText) findViewById(R.id.editNumber);
+        mGroup = (EditText) findViewById(R.id.editGroup);
         mButtSave = (Button) findViewById(R.id.button);
+        mButtPrint = (Button) findViewById(R.id.buttonprint);
 
 
         //Creates
@@ -50,6 +54,7 @@ public class MainActivity extends AppCompatActivity  {
         db.addGroup(new Group("Prijatelji"));
         db.addGroup(new Group("Posao"));
         db.addGroup(new Group("Obitelj"));
+
 
 
 
@@ -74,7 +79,13 @@ public class MainActivity extends AppCompatActivity  {
 
     }
 
-    public Boolean writePdf(Contact contact, Group group)  {
+    public void onPrintContact(View view){
+       // writePdf();
+        createPDF();
+    }
+
+
+    public Boolean writePdf()  {
         try {
             String fpath = "/sdcard/" + "Imenik" + ".pdf";
             File file = new File(fpath);
@@ -83,18 +94,18 @@ public class MainActivity extends AppCompatActivity  {
                 file.createNewFile();
             }
 
+
             // step 1
-            Document document = new Document();
+            Document document = new Document(PageSize.A4.rotate());
             document.addAuthor("Tihomir Vanjurek");
             document.addTitle("Phonebook");
             // step 2
-            PdfWriter.getInstance(document,
-                    new FileOutputStream(file.getAbsoluteFile()));
+            PdfWriter.getInstance(document, new FileOutputStream(file.getAbsoluteFile()));
             // step 3
             document.open();
             // step 4
             document.add(new Paragraph("Hello this is PhoneBook!"));
-            document.add(new Paragraph(contact.getName() + ' ' + contact.getSurname()+ ' ' + contact.getPhoneNumber()+ ' ' + group.getName() ));
+            document.add(new Paragraph("Content"));
             // step 5
             document.close();
 
@@ -108,7 +119,50 @@ public class MainActivity extends AppCompatActivity  {
             e.printStackTrace();
             return false;
         }
+
     }
+
+
+    public void createPDF()
+    {
+        Document doc = new Document();
+
+        try {
+        //    String path = Environment.getExternalStorageDirectory() + "/Imenik" + ".pdf";
+            File f = File.createTempFile( "imenik", ".pdf", getExternalCacheDir());
+            f.createNewFile();
+            f.getAbsolutePath();
+          //  if(!dir.exists())
+          //      dir.mkdirs();
+
+         //   Log.d("PDFCreator", "PDF Path: " + path);
+         //   File file = new File(dir, "Imenik.pdf");
+            FileOutputStream fOut = new FileOutputStream(f);
+            PdfWriter.getInstance(doc, fOut);
+            //open the document
+            doc.open();
+            /* Create Paragraph and Set Font */
+            Paragraph p1 = new Paragraph("Hi! I am Generating my first PDF using DroidText");
+            //add paragraph to document
+            doc.add(p1);
+            Paragraph p2 = new Paragraph("This is an example of a simple paragraph");
+            doc.add(p2);
+
+
+
+        } catch (DocumentException de) {
+            Log.e("PDFCreator", "DocumentException:" + de);
+        } catch (IOException e) {
+            Log.e("PDFCreator", "ioException:" + e);
+        }
+        finally
+        {
+            doc.close();
+        }
+    }
+
+
+
 
 
 
